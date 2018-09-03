48e8bea4862f37c13acc1e8ec0c2641a577b170f
Maximiliano D. Vázquez <mvazquez@veritran.com>
2018-09-03 20:11:58 -0300

    Servicio test

--- beAlmacen/server.js
+++ beAlmacen/server.js
@@ -5,6 +5,8 @@ const bodyParser     = require('body-parser');
 const app            = express();
 
 const port = 8082;
+
+require('./app/routes')(app, {});
 app.listen(port, () => {
   console.log('We are live on ' + port);
 });
